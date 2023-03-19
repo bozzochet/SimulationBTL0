4812c37bba76f08f4bd7e545f293bf59c3ea5f12
Matteo Duranti <bozzo1983@gmail.com>
2023-03-19 19:36:49 +0100

    now also for U is working. Still resolutions are systematically underestimated

--- SimuBT.C
+++ SimuBT.C
@@ -15,7 +15,63 @@ void Full(int component=0);
 double SingleTrial(int events=1000000, bool kNoDraw=false, int component=0);
 
 //----
- 
+
+/* rotation and anti-rotation:
+   u = x*cosα - y*sinα;
+   v = x*sinα + y*cosα;
+   
+   x = u*cosα + v*sinα;
+   y = -u*sinα + v*cosα;
+*/
+
+/* mixed:
+   x = f(u,y) = u*cosα + (x*sinα+y*cosα)*sinα;
+   x = u*cosα + x*sin^2α + y*cosα*sinα;
+   x (1 - sin^2α) = u*cosα + y*cosα*sinα;
+   x = (u*cosα + y*cosα*sinα)/(1 - sin^2α);
+
+   ...
+
+   v = f(u, y) = (u*cosα + v*sinα)*sinα + y*cosα;
+   v = u*cosα*sinα + v*sin^2α + y*cosα;
+   v (1 - sin^2α) = u*cosα*sinα + y*cosα;
+   v = (u*cosα*sinα + y*cosα)/(1 - sin^2α);
+*/
+
+double Rotation(double x, double y, double angle, int component){
+
+  if (component==0) { //u
+    return x*TMath::Cos(TMath::DegToRad()*angle) - y*TMath::Sin(TMath::DegToRad()*angle);
+  }
+  else if (component==1) {//v
+    return x*TMath::Sin(TMath::DegToRad()*angle) + y*TMath::Cos(TMath::DegToRad()*angle);
+  }
+  else {
+    printf("Rotation) Wrong component (%d)...\n", component);
+  }
+  
+  return -999.9;  
+}
+
+double MixedRotation(double a, double b, double angle, int component){
+
+  if (component==0) { //x from u and y
+    double u = a;
+    double y = b;
+    return (u*TMath::Cos(TMath::DegToRad()*angle) + y*TMath::Cos(TMath::DegToRad()*angle)*TMath::Sin(TMath::DegToRad()*angle))/(1.0 - TMath::Power(TMath::Sin(TMath::DegToRad()*angle), 2.0));
+  }
+  else if (component==3) {//v from u and y
+    double u = a;
+    double y = b;
+    return (u*TMath::Cos(TMath::DegToRad()*angle)*TMath::Sin(TMath::DegToRad()*angle) + y*TMath::Cos(TMath::DegToRad()*angle))/(1.0 - TMath::Power(TMath::Sin(TMath::DegToRad()*angle), 2.0));
+  }
+  else {
+    printf("MixedRotation) Wrong or not yet implemented component (%d)...\n", component);
+  }
+  
+  return -999.9;  
+}
+
 void Single(){
   SingleTrial();
 }
@@ -93,12 +149,18 @@ double SingleTrial(int events, bool kNoDraw, int component) {
   TF1* fitX = new TF1("fitX", "[0] + [1]*x", 0.0, 2.0);
   TF1* fitY = new TF1("fitY", "[0] + [1]*x", 0.0, 2.0);
 
-  TF1* smegaus = new TF1("smegaus", "[0]*exp(-0.5*((x-[1])/[2])*((x-[1])/[2]))", -50, 50);
-  smegaus->SetLineColor(kBlack);
-  smegaus->SetLineWidth(3);
-  TF1* resgaus = new TF1("resgaus", "[0]*exp(-0.5*((x-[1])/[2])*((x-[1])/[2]))", -50, 50);
-  resgaus->SetLineColor(kRed+2);
-  resgaus->SetLineWidth(3);
+  TF1* smegausX = new TF1("smegausX", "[0]*exp(-0.5*((x-[1])/[2])*((x-[1])/[2]))", -50, 50);
+  smegausX->SetLineColor(kBlack);
+  smegausX->SetLineWidth(3);
+  TF1* smegausY = new TF1("smegausY", "[0]*exp(-0.5*((x-[1])/[2])*((x-[1])/[2]))", -50, 50);
+  smegausY->SetLineColor(kBlue+1);
+  smegausY->SetLineWidth(3);
+  TF1* resgausX = new TF1("resgausX", "[0]*exp(-0.5*((x-[1])/[2])*((x-[1])/[2]))", -50, 50);
+  resgausX->SetLineColor(kRed+2);
+  resgausX->SetLineWidth(3);  
+  TF1* resgausY = new TF1("resgausY", "[0]*exp(-0.5*((x-[1])/[2])*((x-[1])/[2]))", -50, 50);
+  resgausY->SetLineColor(kRed+2);
+  resgausY->SetLineWidth(3);
   
   TH1* mctBeamPipeX = new TH1F("mctBeamPipeX", "mctBeamPipeX", 1000, -10.0, 10.0);
   mctBeamPipeX->GetXaxis()->SetTitle("X-coo at Beam Pipe (mm)");
@@ -171,6 +233,7 @@ double SingleTrial(int events, bool kNoDraw, int component) {
   TH1* fitDUT[nDUT];
   TH1* fitDUTSmeX[nDUT];
   TH1* fitDUTResX[nDUT];
+  TH1* fitDUTSmeY[nDUT];
   for (int ii=0; ii<nDUT; ii++) {
     fitDUT[ii] = NULL;
     if (DUTK[ii] == "X") {
@@ -191,6 +254,11 @@ double SingleTrial(int events, bool kNoDraw, int component) {
     fitDUTResX[ii]->GetYaxis()->SetTitle("Entries");
     fitDUTResX[ii]->SetLineColor(kRed+2);
     fitDUTResX[ii]->SetMarkerColor(kRed+2);
+    fitDUTSmeY[ii] = new TH1F(Form("fitDUTSmeY_%d", ii), Form("fitDUTSmeY_%d", ii), 1000, -reslim, reslim);
+    fitDUTSmeY[ii]->GetXaxis()->SetTitle("Y-coo residual at DUT (#mum)");
+    fitDUTSmeY[ii]->GetYaxis()->SetTitle("Entries");
+    fitDUTSmeY[ii]->SetLineColor(kBlue+1);
+    fitDUTSmeY[ii]->SetMarkerColor(kBlue+1);
   }
     
   for (int ii=0; ii<events; ii++) {
@@ -224,7 +292,7 @@ double SingleTrial(int events, bool kNoDraw, int component) {
       else if (DUTK[ii] == "U") {
 	double x = mctX->Eval(DUTZ[ii]);
 	double y = mctY->Eval(DUTZ[ii]);
-	double u = x*TMath::Cos(TMath::DegToRad()*rotU) - y*TMath::Sin(TMath::DegToRad()*rotU);
+	double u = Rotation(x, y, rotU, 0);
 	mctDUT[ii]->Fill(u);
       }
     }
@@ -281,35 +349,34 @@ double SingleTrial(int events, bool kNoDraw, int component) {
 	double xtruth = mctX->Eval(DUTZ[ii]);
 	double xtrack = fitX->Eval(DUTZ[ii]);
 	double xmeas = gRandom->Gaus(xtruth, resoX);
+	double ytruth = mctY->Eval(DUTZ[ii]);
+	double ytrack = fitY->Eval(DUTZ[ii]);
 	fitDUT[ii]->Fill(xtrack);
 	fitDUTSmeX[ii]->Fill(1000.0*(xtrack - xtruth));
 	fitDUTResX[ii]->Fill(1000.0*(xmeas - xtrack));
+	fitDUTSmeY[ii]->Fill(1000.0*(ytrack - ytruth));
       }
       else if (DUTK[ii] == "U") {
 	double xtruth = mctX->Eval(DUTZ[ii]);
 	double xtrack = fitX->Eval(DUTZ[ii]);
 	double ytruth = mctY->Eval(DUTZ[ii]);
 	double ytrack = fitY->Eval(DUTZ[ii]);
-	double utruth = xtruth*TMath::Cos(TMath::DegToRad()*rotU) -
-	  ytruth*TMath::Sin(TMath::DegToRad()*rotU);
-	double utrack = xtrack*TMath::Cos(TMath::DegToRad()*rotU) -
-	  ytrack*TMath::Sin(TMath::DegToRad()*rotU);
-	double vtruth = xtruth*TMath::Sin(TMath::DegToRad()*rotU) +
-	  ytruth*TMath::Cos(TMath::DegToRad()*rotU);
-	double vtrack = xtrack*TMath::Sin(TMath::DegToRad()*rotU) +
-	  ytrack*TMath::Cos(TMath::DegToRad()*rotU);
+	double utruth = Rotation(xtruth, ytruth, rotU, 0);
+	double utrack = Rotation(xtrack, ytrack, rotU, 0);
+	double vtruth = Rotation(xtruth, ytruth, rotU, 1);
+	double vtrack = Rotation(xtrack, ytrack, rotU, 1);
 	double umeas = gRandom->Gaus(utruth, resoU);
 	// we need a "v":
 	//	double vmeas = vtruth; //v from truth
 	//	double vmeas = vtrack; //v from track
-	double vmeas = gRandom->Gaus(vtruth, resoU); //v from another layer
-	double xmeas = umeas*TMath::Cos(TMath::DegToRad()*rotU) +
-	  vmeas*TMath::Sin(TMath::DegToRad()*rotU);
-	double ymeas = -umeas*TMath::Sin(TMath::DegToRad()*rotU) +
-	  vmeas*TMath::Cos(TMath::DegToRad()*rotU);
+	//	double vmeas = gRandom->Gaus(vtruth, resoU); //v from another layer
+	double vmeas = MixedRotation(umeas, ytrack, rotU, 3);//from u and y
+	double xmeas = Rotation(umeas, vmeas, -rotU, 0);
+	double ymeas = Rotation(umeas, vmeas, -rotU, 1);
 	fitDUT[ii]->Fill(utrack);
 	fitDUTSmeX[ii]->Fill(1000.0*(xtrack - xtruth));
 	fitDUTResX[ii]->Fill(1000.0*(xmeas - xtrack));
+	fitDUTSmeY[ii]->Fill(1000.0*(ytrack - ytruth));
       }
     }
 
@@ -368,19 +435,35 @@ double SingleTrial(int events, bool kNoDraw, int component) {
   double resoXmeas[nDUT];
   
   for (int ii=0; ii<nDUT; ii++) {
+    
     if (!kNoDraw) {
       TCanvas* cdsx = new TCanvas(Form("DUTSmearingX_%d", ii), Form("DUTSmearingX_%d", ii));
       fitDUTSmeX[ii]->Draw();
     }
-    smegaus->SetParameter(0, fitDUTSmeX[ii]->GetMaximum());
-    smegaus->SetParameter(1, fitDUTSmeX[ii]->GetMean());
-    smegaus->SetParLimits(2, 0.0, 10.0*fitDUTSmeX[ii]->GetRMS());
-    smegaus->SetParameter(2, fitDUTSmeX[ii]->GetRMS());
+    smegausX->SetParameter(0, fitDUTSmeX[ii]->GetMaximum());
+    smegausX->SetParameter(1, fitDUTSmeX[ii]->GetMean());
+    smegausX->SetParLimits(2, 0.0, 10.0*fitDUTSmeX[ii]->GetRMS());
+    smegausX->SetParameter(2, fitDUTSmeX[ii]->GetRMS());
     if (kNoDraw) {
-      fitDUTSmeX[ii]->Fit(smegaus, "NQ", "L");
+      fitDUTSmeX[ii]->Fit(smegausX, "NQ", "L");
     }
     else {
-      fitDUTSmeX[ii]->Fit(smegaus, "Q", "L");
+      fitDUTSmeX[ii]->Fit(smegausX, "Q", "L");
+    }
+
+    if (!kNoDraw) {
+      TCanvas* cdsy = new TCanvas(Form("DUTSmearingY_%d", ii), Form("DUTSmearingY_%d", ii));
+      fitDUTSmeY[ii]->Draw();
+    }
+    smegausY->SetParameter(0, fitDUTSmeY[ii]->GetMaximum());
+    smegausY->SetParameter(1, fitDUTSmeY[ii]->GetMean());
+    smegausY->SetParLimits(2, 0.0, 10.0*fitDUTSmeY[ii]->GetRMS());
+    smegausY->SetParameter(2, fitDUTSmeY[ii]->GetRMS());
+    if (kNoDraw) {
+      fitDUTSmeY[ii]->Fit(smegausY, "NQ", "L");
+    }
+    else {
+      fitDUTSmeY[ii]->Fit(smegausY, "Q", "L");
     }
     
     if (!kNoDraw) {
@@ -388,27 +471,46 @@ double SingleTrial(int events, bool kNoDraw, int component) {
       fitDUTResX[ii]->GetYaxis()->SetRangeUser(0, 1.1*fitDUTSmeX[ii]->GetMaximum());
       fitDUTResX[ii]->Draw();
     }
-    resgaus->SetParameter(0, fitDUTResX[ii]->GetMaximum());
-    resgaus->SetParameter(1, fitDUTResX[ii]->GetMean());
-    resgaus->SetParLimits(2, 0.0, 10.0*fitDUTResX[ii]->GetRMS());
-    resgaus->SetParameter(2, fitDUTResX[ii]->GetRMS());
+    resgausX->SetParameter(0, fitDUTResX[ii]->GetMaximum());
+    resgausX->SetParameter(1, fitDUTResX[ii]->GetMean());
+    resgausX->SetParLimits(2, 0.0, 10.0*fitDUTResX[ii]->GetRMS());
+    resgausX->SetParameter(2, fitDUTResX[ii]->GetRMS());
     if (kNoDraw) {
-      fitDUTResX[ii]->Fit(resgaus, "NQ", "L");
+      fitDUTResX[ii]->Fit(resgausX, "NQ", "L");
     }
     else {
-      fitDUTResX[ii]->Fit(resgaus, "Q", "L");
+      fitDUTResX[ii]->Fit(resgausX, "Q", "L");
       fitDUTSmeX[ii]->Draw("same");
+      if (DUTK[ii] == "U") {
+	fitDUTSmeY[ii]->Draw("same");
+      }
     }
     
     //--------------
     
-    double sigmaXmeas = resgaus->GetParameter(2);
-    double smeaXmeas = smegaus->GetParameter(2);
-    
-    resoXmeas[ii] = TMath::Sqrt(TMath::Power(sigmaXmeas, 2.0) - TMath::Power(smeaXmeas, 2.0));
-    if (!kNoDraw) {
-      printf("DUT X %d) sigma measured: %f, smearing measured: %f\n", ii, sigmaXmeas, smeaXmeas);
-      printf("DUT X %d) resolution: %f\n", ii, resoXmeas[ii]);
+    double sigmaXmeas = resgausX->GetParameter(2);
+    double sigmaYmeas = resgausY->GetParameter(2);
+    double smeaYmeas = smegausY->GetParameter(2);
+    double smeaXmeas = smegausX->GetParameter(2);
+
+    if (DUTK[ii] == "X") {
+      resoXmeas[ii] = TMath::Sqrt(TMath::Power(sigmaXmeas, 2.0) - TMath::Power(smeaXmeas, 2.0));
+      if (!kNoDraw) {
+	printf("DUT X %d) ", ii);
+	printf("sigma measured: %f, ", sigmaXmeas);
+	printf("smearing measured: %f\n", smeaXmeas);
+	printf("--> resolution: %f\n", resoXmeas[ii]);
+      }
+    }
+    else if (DUTK[ii] == "U") {
+      resoXmeas[ii] = TMath::Sqrt(TMath::Power(sigmaXmeas, 2.0) - TMath::Power(smeaXmeas, 2.0) - TMath::Power(smeaYmeas, 2.0));
+      if (!kNoDraw) {
+	printf("DUT X %d) ", ii);
+	printf("sigma measured: %f, ", sigmaXmeas);
+	printf("smearing X measured: %f, ", smeaXmeas);
+	printf("smearing Y measured: %f\n", smeaYmeas);
+	printf("--> resolution: %f\n", resoXmeas[ii]);
+      }
     }
   }
 
@@ -420,8 +522,10 @@ double SingleTrial(int events, bool kNoDraw, int component) {
     if (fitX) delete fitX;
     if (fitY) delete fitY;
     
-    if (smegaus) delete smegaus;
-    if (resgaus) delete resgaus;
+    if (smegausX) delete smegausX;
+    if (smegausY) delete smegausY;
+    if (resgausX) delete resgausX;
+    if (resgausY) delete resgausY;
     
     if (mctBeamPipeX) delete mctBeamPipeX;
     if (mctBeamPipeY) delete mctBeamPipeY;
@@ -449,6 +553,7 @@ double SingleTrial(int events, bool kNoDraw, int component) {
       if (fitDUT[ii]) delete fitDUT[ii];
 
       if (fitDUTSmeX[ii]) delete fitDUTSmeX[ii];
+      if (fitDUTSmeY[ii]) delete fitDUTSmeY[ii];
       if (fitDUTResX[ii]) delete fitDUTResX[ii];
     }
   }
